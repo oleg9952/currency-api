cd9003f5abce7400bf1111c4864927377eacf3aa
Oleh Parkhomenko <oleh_parkhomenko@epam.com>
2020-05-27 23:14:08 +0300

    enabled CORS for heroku

--- server.js
+++ server.js
@@ -3,6 +3,12 @@ const app = express()
 const fetch = require('node-fetch')
 const PORT = process.env.PORT || 4500
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+    next()
+})
+
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
